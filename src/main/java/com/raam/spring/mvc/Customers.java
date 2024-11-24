7f6cea4d4390b0a82ed8132bfd907f9432fbc004
RaamTechLead-DSAI <braamprekash@gmail.com>
2024-11-24 19:02:19 +0000

    First Commit

---
+++ src/main/java/com/raam/spring/mvc/Customers.java
@@ -0,0 +1,22 @@
+package com.raam.spring.mvc;
+
+public class Customers {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
